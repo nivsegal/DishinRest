af2686e197610af8f03eebf63edf7506319d1fe1
Niv <nivsega@gmail.com>
2018-02-22 23:08:52 +0200

    fixed the controller to bring only the appropriate restaurant + added a bad favicon

--- api/controllers/RestaurantController.js
+++ api/controllers/RestaurantController.js
@@ -8,31 +8,22 @@
 module.exports = {
 	create: (req, res) => {
 		const inputs = req.allParams();
-		console.log(req.user);
 		Restaurant.create(inputs)
-		.then(restaurant => {
-			console.log('restaurant',restaurant)
-			return User.find(req.user.id)
-		.then(user => {
-			console.log('user', user);
-			restaurant.owners.add(req.user);
-			return restaurant.save();
-		})
-		})
-		.then(updated =>{
-			console.log('here', updated)
-			return res.json('success');
-		})
-		.catch(err => console.error(err));
+			.then(restaurant => {
+				return User.find(req.user.id)
+					.then(user => {
+						restaurant.owners.add(req.user);
+						return restaurant.save();
+					})
+			})
+			.then(updated => {
+				return res.json('success');
+			})
+			.catch(err => console.error(err));
 	},
 
 	index: (req, res) => {
-		Restaurant.find().populate('tags').populate('menus').populate('owners').then(restaurants => {
-			restaurants.filter(restaurant =>{
-				return restaurant.owners.filter(user => {
-					return user.id === req.user.id;
-				})
-			})
+		Restaurant.find().populate('tags').populate('menus').populate('owners', { id: req.user.id }).then(restaurants => {
 			return res.view('user/restaurant', { layout: 'user/layout', restaurants });
 		});
 	}
